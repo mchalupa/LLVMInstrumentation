2809ae059f21d32e6aa2696c02d8318905e59828
Marek Chalupa <mchqwerty@gmail.com>
2017-08-31 12:52:20 +0200

    rewriter: refactor
    Split into logically structured functions (as was suggested by comments
    for future work)
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- src/rewriter.cpp
+++ src/rewriter.cpp
@@ -7,9 +7,114 @@
 
 using namespace std;
 
+static void getFindInstructions(RewriteRule& r, const Json::Value& instruction_rules)
+{
+    const auto& find_instructions = instruction_rules["findInstructions"];
+
+    for (uint k = 0; k < find_instructions.size(); ++k) {
+        InstrumentInstruction instr;
+        const auto& instruction = find_instructions[k];
+        instr.returnValue = instruction["returnValue"].asString();
+        instr.instruction = instruction["instruction"].asString();
+
+        const auto& instruction_operands = instruction["operands"];
+        for (uint j = 0; j < instruction_operands.size(); ++j) {
+            instr.parameters.emplace_back(instruction_operands[j].asString());
+        }
+
+        instr.getSizeTo = instruction["getSizeTo"].asString();
+        instr.stripInboundsOffsets = instruction["stripInboundsOffsets"].asString();
+        r.foundInstrs.emplace_back(std::move(instr));
+    }
+}
+
+static void getNewInstruction(RewriteRule& r, const Json::Value& instruction_rules)
+{
+    const auto& new_instruction = instruction_rules["newInstruction"];
+    r.newInstr.returnValue = new_instruction["returnValue"].asString();
+    r.newInstr.instruction = new_instruction["instruction"].asString();
+
+    const auto& new_instruction_operands = new_instruction["operands"];
+    for (uint j = 0; j < new_instruction_operands.size(); ++j) {
+        r.newInstr.parameters.emplace_back(new_instruction_operands[j].asString());
+    }
+
+    const auto& instruction_placement = instruction_rules["where"];
+    if (instruction_placement == "before") {
+        r.where = InstrumentPlacement::BEFORE;
+    }
+    else if (instruction_placement == "after") {
+        r.where = InstrumentPlacement::AFTER;
+    }
+    else if (instruction_placement == "replace") {
+        r.where = InstrumentPlacement::REPLACE;
+    }
+    else if (instruction_placement == "return") {
+        r.where = InstrumentPlacement::RETURN;
+    }
+    else if (instruction_placement == "entry") {
+        r.where = InstrumentPlacement::ENTRY;
+    }
+
+    r.inFunction = instruction_rules["in"].asString();
+
+    const auto& instr_condition = instruction_rules["condition"];
+    for(uint j = 0; j < instr_condition.size(); ++j){
+        r.condition.emplace_back(instr_condition[j].asString());
+    }
+}
+
+static RewritePhase getPhase(const Json::Value& current_phase)
+{
+    RewritePhase phase;
+
+    /// ------------------------------------
+    // Get instruction rules.
+    /// ------------------------------------
+    const auto& instruction_rules = current_phase["instructionRules"];
+    for (uint i = 0; i < instruction_rules.size(); ++i) {
+        RewriteRule r;
+        const auto& current_instruction_rules = instruction_rules[i];
+
+        getFindInstructions(r, current_instruction_rules);
+        getNewInstruction(r, current_instruction_rules);
+
+        phase.getConfig().emplace_back(std::move(r));
+    }
+
+    /// ------------------------------------
+    // Get rule for global variables.
+    /// ------------------------------------
+    GlobalVarsRule& rw_globals_rule = phase.getGlobalsConfig();
+
+    const auto& global_variables_rule = current_phase["globalVariablesRule"];
+    const auto& find_globals = global_variables_rule["findGlobals"];
+    rw_globals_rule.globalVar.globalVariable = find_globals["globalVariable"].asString();
+    rw_globals_rule.globalVar.getSizeTo = find_globals["getSizeTo"].asString();
+
+    const auto& gv_condition = global_variables_rule["condition"];
+    for(uint j = 0; j < gv_condition.size(); ++j){
+        rw_globals_rule.condition.emplace_back(gv_condition[j].asString());
+    }
+
+    const auto& gv_new_instruction = global_variables_rule["newInstruction"];
+    rw_globals_rule.newInstr.returnValue = gv_new_instruction["returnValue"].asString();
+    rw_globals_rule.newInstr.instruction = gv_new_instruction["instruction"].asString();
+
+    const auto& gv_new_instruction_operands = gv_new_instruction["operands"];
+    for (uint j = 0; j < gv_new_instruction_operands.size(); ++j) {
+        rw_globals_rule.newInstr.parameters.emplace_back(gv_new_instruction_operands[j].asString());
+    }
+
+    rw_globals_rule.inFunction = global_variables_rule["in"].asString();
+
+    return phase;
+}
+
 void Rewriter::parseConfig(ifstream &config_file) {
     Json::Value json_rules;
     Json::Reader reader;
+
     bool parsingSuccessful = reader.parse(config_file, json_rules);
     if (!parsingSuccessful)    {
         cerr  << "Failed to parse configuration\n"
@@ -20,8 +125,9 @@ void Rewriter::parseConfig(ifstream &config_file) {
     // TODO catch exceptions here
 
     // load paths to analyses
-    for(uint i = 0; i < json_rules["analyses"].size(); ++i){
-        this->analysisPaths.push_back(json_rules["analyses"][i].asString());
+    const auto& analyses = json_rules["analyses"];
+    for(uint i = 0; i < analyses.size(); ++i){
+        this->analysisPaths.emplace_back(analyses[i].asString());
     }
 
     const auto& config_phases = json_rules["phases"];
@@ -31,87 +137,10 @@ void Rewriter::parseConfig(ifstream &config_file) {
     }
 
     for (uint phase_idx = 0; phase_idx < config_phases.size(); ++phase_idx) {
-        RewritePhase phase;
-        
+
         // load rewrite rules for instructions
         const auto& current_phase = config_phases[phase_idx];
-        const auto& instruction_rules = current_phase["instructionRules"];
-        for (uint i = 0; i < instruction_rules.size(); ++i) {
-            RewriteRule r;
-        
-            // TODO make function from this
-            // Get findInstructions
-            const auto& find_instructions = instruction_rules[i]["findInstructions"];
-            for (uint k = 0; k < find_instructions.size(); ++k) {
-                InstrumentInstruction instr;
-                instr.returnValue = find_instructions[k]["returnValue"].asString();
-                instr.instruction = find_instructions[k]["instruction"].asString();
-                for (uint j = 0; j < find_instructions[k]["operands"].size(); ++j) {
-                    instr.parameters.push_back(find_instructions[k]["operands"][j].asString());
-                }
-                instr.getSizeTo = find_instructions[k]["getSizeTo"].asString();
-                instr.stripInboundsOffsets = find_instructions[k]["stripInboundsOffsets"].asString();
-                r.foundInstrs.push_back(instr);
-            }
-        
-            // Get newInstruction
-            r.newInstr.returnValue = instruction_rules[i]["newInstruction"]["returnValue"].asString();
-            r.newInstr.instruction = instruction_rules[i]["newInstruction"]["instruction"].asString();
-            for (uint j = 0; j < instruction_rules[i]["newInstruction"]["operands"].size(); ++j) {
-                r.newInstr.parameters.push_back(instruction_rules[i]["newInstruction"]["operands"][j].asString());
-            }
-        
-            const auto& instruction_placement = instruction_rules[i]["where"];
-            if (instruction_placement == "before") {
-                r.where = InstrumentPlacement::BEFORE;
-            }
-            else if (instruction_placement == "after") {
-                r.where = InstrumentPlacement::AFTER;
-            }
-            else if (instruction_placement == "replace") {
-                r.where = InstrumentPlacement::REPLACE;
-            }
-            else if (instruction_placement == "return") {
-                r.where = InstrumentPlacement::RETURN;
-            }
-            else if (instruction_placement == "entry") {
-                r.where = InstrumentPlacement::ENTRY;
-            }
-        
-            r.inFunction = instruction_rules[i]["in"].asString();
-        
-            for(uint j = 0; j < instruction_rules[i]["condition"].size(); ++j){
-                r.condition.push_back(instruction_rules[i]["condition"][j].asString());
-            }
-        
-            phase.getConfig().push_back(r);
-        }
-        
-        GlobalVarsRule& rw_globals_rule = phase.getGlobalsConfig();
-        
-        // Get rule for global variables
-        const auto& global_variables_rule = current_phase["globalVariablesRule"];
-        const auto& find_globals = global_variables_rule["findGlobals"];
-        rw_globals_rule.globalVar.globalVariable = find_globals["globalVariable"].asString();
-        rw_globals_rule.globalVar.getSizeTo = find_globals["getSizeTo"].asString();
-        
-        const auto& gv_condition = global_variables_rule["condition"];
-        for(uint j = 0; j < gv_condition.size(); ++j){
-            rw_globals_rule.condition.push_back(gv_condition[j].asString());
-        }
-        
-        const auto& gv_new_instruction = global_variables_rule["newInstruction"];
-        rw_globals_rule.newInstr.returnValue = gv_new_instruction["returnValue"].asString();
-        rw_globals_rule.newInstr.instruction = gv_new_instruction["instruction"].asString();
-        
-        const auto& gv_new_instruction_operands = gv_new_instruction["operands"];
-        for (uint j = 0; j < gv_new_instruction_operands.size(); ++j) {
-            rw_globals_rule.newInstr.parameters.push_back(gv_new_instruction_operands[j].asString());
-        }
-        
-        rw_globals_rule.inFunction = global_variables_rule["in"].asString();
-        
-        phases.push_back(phase);
+        phases.emplace_back(getPhase(current_phase));
     }
 }
 
